aee9ae1335d90def3c17b598c2f71c4609129808
oreotamish <sinhatamish@gmail.com>
2023-10-11 12:25:42 +0530

    Implemented buffer to array logic

---
+++ src/helper.rs
@@ -0,0 +1,23 @@
+use bytes::Buf;
+
+fn buffer_to_array(buf: &mut BytesMut) -> Vec<String> {
+    let mut vec = vec![];
+    let length = buf.len();
+    let mut word = "".to_string();
+
+    for i in 0..length {
+        match buf.get_u8() {
+            b' '=> {
+                vec.push(word);
+                word = "".to_string();
+            }
+            other => {
+                word.push(other as char);
+                let new = word.clone();
+                if i == length - 1 {
+                    vec.push(new);
+                }
+            }
+        }
+    }
+}

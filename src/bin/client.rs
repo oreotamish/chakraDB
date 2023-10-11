ad1353dad88fafc47f0b985bed68bd01ddbe4f6c
oreotamish <sinhatamish@gmail.com>
2023-10-11 13:46:31 +0530

    Client handling for commands

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -3,8 +3,19 @@ use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
-    let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
-    stream.write_all(b"set foo bar").await?;
-
-    Ok(())
+    let mut buf = BytesMut::with_capacity(1024);
+    let _length = stream.read_buf(&mut buf).await?;
+    match std::str::from_utf8(&mut buf) {
+        Ok(resp) => {
+            if resp == "r Ok" {
+                println!("key updated");
+            } else if resp == "Ok" {
+                println!("key set");
+            }
+        }
+        Err(err: Utf8Error) => {
+            println!("error: {}", err);
+        }
+    }
+    
 }

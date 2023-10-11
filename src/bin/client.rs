026ef5baec145a6e897e80405f6c6df7f554a0f5
oreotamish <sinhatamish@gmail.com>
2023-10-11 12:04:26 +0530

    client test stream write

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, client!");
+use tokio::net::TcpStream;
+use tokio::io::AsyncWriteExt;
+
+#[tokio::main]
+pub async fn main() -> Result<(), std::io::Error> {
+    let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
+    stream.write_all(b"set foo bar").await?;
+
+    Ok(())
 }

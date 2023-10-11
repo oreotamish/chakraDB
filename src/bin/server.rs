ebf1b83b772fb5da2a602f45ac44f8bf586b6344
oreotamish <sinhatamish@gmail.com>
2023-10-11 14:29:30 +0530

    final changes server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,52 +1,39 @@
-use tokio::net::TcpListener;
-use bytes::BytesMut;
+use chakraDB::server;
+use chakraDB::Listener;
+use tokio::signal;
+use tokio::{
+    net::TcpListener,
+    sync::{broadcast, mpsc},
+};
 
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8081").await?;
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-        println!("Accepted connection from {:?}", socket);
-        let mut buf = BytesMut::with_capacity(1024);
-        let _ = socket.try_read_buf(&mut buf);
-        let attrs = buffer_to_array(&mut buf);
-        let command = Command::get_command(&attrs[0]);
-        println!("buffer {:?}", attrs);
-        process_query(command, attrs, &mut socket, &mut db).await?;
-        
-    }
-}
+    let shutdown = signal::ctrl_c();
+    let (notify_shutdown, _) = broadcast::channel(1);
+    let (shutdown_complete_tx, shutdown_complete_rx) = mpsc::channel(1);
 
-async fn process_query(
-    command: Command,
-    attrs: Vec<String>,
-    socket: &mut TcpStream,
-    db: &mut Db,
-) -> std::io::Result<()> {
-    match command {
-        Command::Get => {
-            Ok(())
-        }
-        Command::Set => {
-            let resp = db.write(&attrs);
-
-            match resp {
-                Ok(result) => {
-                    println!("set result: {}", result);
-                    socket.write_all(&result.as_bytes()).await?;
-                }
-                Err(_err) => {
-                    socket.write_all(b"").await?;
-                }
+    let mut listener = Listener::new(
+        listener,
+        notify_shutdown,
+        shutdown_complete_tx,
+        shutdown_complete_rx,
+    );
+    tokio::select! {
+        res = server::run(&mut listener) => {
+             if let Err(_err) = res {
+               println!("failed to accept connection");
             }
-
-            Ok(())
         }
-        Command::Invalid => Ok(()),
-    
+        _ = shutdown => {
+            println!("inside shutdown loop");
+        }
+    }
+    drop(listener.notify_shutdown);
+    drop(listener.shutdown_complete_tx);
+    println!("before final shutdown");
+    let _ = listener.shutdown_complete_rx.recv().await;
+    println!("after final shutdown");
 
-let db = Db::new()
-loop {
-    let (socket, _) = listener.accept().await?;
+    Ok(())
 }
-

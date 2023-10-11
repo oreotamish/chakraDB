68a42096764877f0d19d53ca24d91c4cbcac975e
oreotamish <sinhatamish@gmail.com>
2023-10-11 11:49:36 +0530

    Feat: socket listener on 8081 server implementation

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, server!");
+use tokio::net::TcpListener
+
+#[tokio::main]
+pub async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8081").await?;
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        println!("Accepted connection from {:?}", socket);
+    }
 }
+
+
